fcb17d2ac4021e2dfc131e8c41ad695c969c5f88
beicause <1494181792@qq.com>
2024-07-21 13:01:24 +0800

    skeleton2d fabr ik add property

--- scene/2d/skeleton_ik_2d_fabr.cpp
+++ scene/2d/skeleton_ik_2d_fabr.cpp
@@ -55,12 +55,12 @@ void SkeletonIK2DFABR::_process_modification() {
 	}
 	target_global_pose = target->get_global_transform();
 
-	if (fabrik_data_chain[0].bone2d_node_id.is_null() && !fabrik_data_chain[0].bone2d_node.is_empty()) {
+	if (fabrik_data_chain[0].bone_node_id.is_null() && !fabrik_data_chain[0].bone_node.is_empty()) {
 		fabrik_joint_update_node(0);
 		WARN_PRINT("Bone2D cache for origin joint is out of date. Updating...");
 	}
 
-	Bone2D *origin_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[0].bone2d_node_id));
+	Bone2D *origin_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[0].bone_node_id));
 	if (!origin_bone2d_node || !origin_bone2d_node->is_inside_tree()) {
 		ERR_PRINT_ONCE("Origin joint's Bone2D node is not in the scene tree. Cannot execute modification!");
 		return;
@@ -74,11 +74,11 @@ void SkeletonIK2DFABR::_process_modification() {
 
 	for (int i = 0; i < fabrik_data_chain.size(); i++) {
 		// Update the transform chain
-		if (fabrik_data_chain[i].bone2d_node_id.is_null() && !fabrik_data_chain[i].bone2d_node.is_empty()) {
+		if (fabrik_data_chain[i].bone_node_id.is_null() && !fabrik_data_chain[i].bone_node.is_empty()) {
 			WARN_PRINT_ONCE("Bone2D cache for joint " + itos(i) + " is out of date.. Attempting to update...");
 			fabrik_joint_update_node(i);
 		}
-		Bone2D *joint_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[i].bone2d_node_id));
+		Bone2D *joint_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[i].bone_node_id));
 		if (!joint_bone2d_node) {
 			ERR_PRINT_ONCE("FABRIK Joint " + itos(i) + " does not have a Bone2D node set! Cannot execute modification!");
 			return;
@@ -86,9 +86,9 @@ void SkeletonIK2DFABR::_process_modification() {
 		fabrik_transform_chain.write[i] = joint_bone2d_node->get_global_transform();
 	}
 
-	Bone2D *final_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[fabrik_data_chain.size() - 1].bone2d_node_id));
+	Bone2D *final_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[fabrik_data_chain.size() - 1].bone_node_id));
 	float final_bone2d_angle = final_bone2d_node->get_global_rotation();
-	if (fabrik_data_chain[fabrik_data_chain.size() - 1].use_target_rotation) {
+	if (tip_use_target_rotation) {
 		final_bone2d_angle = target_global_pose.get_rotation();
 	}
 	Vector2 final_bone2d_direction = Vector2(Math::cos(final_bone2d_angle), Math::sin(final_bone2d_angle));
@@ -101,7 +101,7 @@ void SkeletonIK2DFABR::_process_modification() {
 		chain_forwards();
 
 		final_bone2d_angle = final_bone2d_node->get_global_rotation();
-		if (fabrik_data_chain[fabrik_data_chain.size() - 1].use_target_rotation) {
+		if (tip_use_target_rotation) {
 			final_bone2d_angle = target_global_pose.get_rotation();
 		}
 		final_bone2d_direction = Vector2(Math::cos(final_bone2d_angle), Math::sin(final_bone2d_angle));
@@ -115,7 +115,7 @@ void SkeletonIK2DFABR::_process_modification() {
 
 	// Apply all of the saved transforms to the Bone2D nodes
 	for (int i = 0; i < fabrik_data_chain.size(); i++) {
-		Bone2D *joint_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[i].bone2d_node_id));
+		Bone2D *joint_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[i].bone_node_id));
 		if (!joint_bone2d_node) {
 			ERR_PRINT_ONCE("FABRIK Joint " + itos(i) + " does not have a Bone2D node set!");
 			continue;
@@ -126,7 +126,7 @@ void SkeletonIK2DFABR::_process_modification() {
 		if (i + 1 < fabrik_data_chain.size()) {
 			chain_trans = chain_trans.looking_at(fabrik_transform_chain[i + 1].get_origin());
 		} else {
-			if (fabrik_data_chain[i].use_target_rotation) {
+			if (tip_use_target_rotation) {
 				chain_trans.set_rotation(target_global_pose.get_rotation());
 			} else {
 				chain_trans = chain_trans.looking_at(target_global_pose.get_origin());
@@ -146,7 +146,7 @@ void SkeletonIK2DFABR::_process_modification() {
 
 void SkeletonIK2DFABR::chain_backwards() {
 	int final_joint_index = fabrik_data_chain.size() - 1;
-	Bone2D *final_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[final_joint_index].bone2d_node_id));
+	Bone2D *final_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[final_joint_index].bone_node_id));
 	Transform2D final_bone2d_trans = fabrik_transform_chain[final_joint_index];
 
 	// Apply magnet position
@@ -159,7 +159,7 @@ void SkeletonIK2DFABR::chain_backwards() {
 
 	// Set the position of the tip bone
 	float final_bone2d_angle = final_bone2d_trans.get_rotation();
-	if (fabrik_data_chain[final_joint_index].use_target_rotation) {
+	if (tip_use_target_rotation) {
 		final_bone2d_angle = target_global_pose.get_rotation();
 	}
 	Vector2 final_bone2d_direction = Vector2(Math::cos(final_bone2d_angle), Math::sin(final_bone2d_angle));
@@ -173,7 +173,7 @@ void SkeletonIK2DFABR::chain_backwards() {
 	while (i >= 1) {
 		Transform2D previous_pose = fabrik_transform_chain[i];
 		i -= 1;
-		Bone2D *current_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[i].bone2d_node_id));
+		Bone2D *current_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[i].bone_node_id));
 		Transform2D current_pose = fabrik_transform_chain[i];
 
 		// Apply magnet position
@@ -198,7 +198,7 @@ void SkeletonIK2DFABR::chain_forwards() {
 	fabrik_transform_chain.write[0] = origin_bone2d_trans;
 
 	for (int i = 0; i < fabrik_data_chain.size() - 1; i++) {
-		Bone2D *current_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[i].bone2d_node_id));
+		Bone2D *current_bone2d_node = Object::cast_to<Bone2D>(ObjectDB::get_instance(fabrik_data_chain[i].bone_node_id));
 		Transform2D current_pose = fabrik_transform_chain[i];
 		Transform2D next_pose = fabrik_transform_chain[i + 1];
 
@@ -212,7 +212,7 @@ void SkeletonIK2DFABR::chain_forwards() {
 	}
 }
 
-void SkeletonIK2DFABR::_setup_nodes() {
+void SkeletonIK2DFABR::_update_bone_id() {
 	for (int i = 0; i < fabrik_data_chain.size(); i++) {
 		fabrik_joint_update_node(i);
 	}
@@ -226,19 +226,19 @@ void SkeletonIK2DFABR::update_target_node() {
 }
 
 void SkeletonIK2DFABR::fabrik_joint_update_node(int p_joint_idx) {
-	fabrik_data_chain.write[p_joint_idx].bone2d_node_id = ObjectID();
-	Bone2D *node = Object::cast_to<Bone2D>(get_skeleton()->get_node(fabrik_data_chain[p_joint_idx].bone2d_node));
-	fabrik_data_chain.write[p_joint_idx].bone2d_node_id = node->get_instance_id();
+	fabrik_data_chain.write[p_joint_idx].bone_node_id = ObjectID();
+	Bone2D *node = Object::cast_to<Bone2D>(get_skeleton()->get_node(fabrik_data_chain[p_joint_idx].bone_node));
+	fabrik_data_chain.write[p_joint_idx].bone_node_id = node->get_instance_id();
 	fabrik_data_chain.write[p_joint_idx].bone_idx = node->get_bone_idx();
 }
 
 void SkeletonIK2DFABR::_skeleton_changed(Skeleton2D *p_old, Skeleton2D *p_new) {
-	_setup_nodes();
+	_update_bone_id();
 }
 void SkeletonIK2DFABR::_notification(int p_what) {
 	switch (p_what) {
 		case NOTIFICATION_READY: {
-			_setup_nodes();
+			_update_bone_id();
 		} break;
 	}
 }
@@ -252,83 +252,111 @@ NodePath SkeletonIK2DFABR::get_target_node() const {
 	return target_node;
 }
 
-void SkeletonIK2DFABR::set_fabrik_data_chain_length(int p_length) {
+void SkeletonIK2DFABR::set_bone_chain_size(int p_length) {
 	fabrik_data_chain.resize(p_length);
-	notify_property_list_changed();
 }
 
-int SkeletonIK2DFABR::get_fabrik_data_chain_length() {
+int SkeletonIK2DFABR::get_bone_chain_size() {
 	return fabrik_data_chain.size();
 }
 
-void SkeletonIK2DFABR::set_fabrik_joint_bone2d_node(int p_joint_idx, const NodePath &p_target_node) {
+void SkeletonIK2DFABR::set_joint_bone(int p_joint_idx, const NodePath &p_bone) {
 	ERR_FAIL_INDEX_MSG(p_joint_idx, fabrik_data_chain.size(), "FABRIK joint out of range!");
-	fabrik_data_chain.write[p_joint_idx].bone2d_node = p_target_node;
+	fabrik_data_chain.write[p_joint_idx].bone_node = p_bone;
 	fabrik_joint_update_node(p_joint_idx);
-
-	notify_property_list_changed();
 }
 
-NodePath SkeletonIK2DFABR::get_fabrik_joint_bone2d_node(int p_joint_idx) const {
+NodePath SkeletonIK2DFABR::get_joint_bone(int p_joint_idx) const {
 	ERR_FAIL_INDEX_V_MSG(p_joint_idx, fabrik_data_chain.size(), NodePath(), "FABRIK joint out of range!");
-	return fabrik_data_chain[p_joint_idx].bone2d_node;
+	return fabrik_data_chain[p_joint_idx].bone_node;
 }
 
-void SkeletonIK2DFABR::set_fabrik_joint_bone_index(int p_joint_idx, int p_bone_idx) {
+void SkeletonIK2DFABR::set_joint_bone_idx(int p_joint_idx, int p_bone_idx) {
 	ERR_FAIL_INDEX_MSG(p_joint_idx, fabrik_data_chain.size(), "FABRIK joint out of range!");
 
 	if (get_skeleton()) {
 		ERR_FAIL_INDEX_MSG(p_bone_idx, get_skeleton()->get_bone_count(), "Passed-in Bone index is out of range!");
 		fabrik_data_chain.write[p_joint_idx].bone_idx = p_bone_idx;
-		fabrik_data_chain.write[p_joint_idx].bone2d_node_id = get_skeleton()->get_bone(p_bone_idx)->get_instance_id();
-		fabrik_data_chain.write[p_joint_idx].bone2d_node = get_skeleton()->get_path_to(get_skeleton()->get_bone(p_bone_idx));
+		fabrik_data_chain.write[p_joint_idx].bone_node_id = get_skeleton()->get_bone(p_bone_idx)->get_instance_id();
+		fabrik_data_chain.write[p_joint_idx].bone_node = get_skeleton()->get_path_to(get_skeleton()->get_bone(p_bone_idx));
 	} else {
 		WARN_PRINT("Cannot verify the FABRIK joint " + itos(p_joint_idx) + " bone index for this modification...");
 		fabrik_data_chain.write[p_joint_idx].bone_idx = p_bone_idx;
 	}
 }
 
-int SkeletonIK2DFABR::get_fabrik_joint_bone_index(int p_joint_idx) const {
+int SkeletonIK2DFABR::get_joint_bone_idx(int p_joint_idx) const {
 	ERR_FAIL_INDEX_V_MSG(p_joint_idx, fabrik_data_chain.size(), -1, "FABRIK joint out of range!");
 	return fabrik_data_chain[p_joint_idx].bone_idx;
 }
 
-void SkeletonIK2DFABR::set_fabrik_joint_magnet_position(int p_joint_idx, Vector2 p_magnet_position) {
+void SkeletonIK2DFABR::set_joint_magnet(int p_joint_idx, Vector2 p_magnet) {
 	ERR_FAIL_INDEX_MSG(p_joint_idx, fabrik_data_chain.size(), "FABRIK joint out of range!");
-	fabrik_data_chain.write[p_joint_idx].magnet_position = p_magnet_position;
+	fabrik_data_chain.write[p_joint_idx].magnet_position = p_magnet;
 }
 
-Vector2 SkeletonIK2DFABR::get_fabrik_joint_magnet_position(int p_joint_idx) const {
+Vector2 SkeletonIK2DFABR::get_joint_magnet(int p_joint_idx) const {
 	ERR_FAIL_INDEX_V_MSG(p_joint_idx, fabrik_data_chain.size(), Vector2(), "FABRIK joint out of range!");
 	return fabrik_data_chain[p_joint_idx].magnet_position;
 }
 
-void SkeletonIK2DFABR::set_fabrik_joint_use_target_rotation(int p_joint_idx, bool p_use_target_rotation) {
-	ERR_FAIL_INDEX_MSG(p_joint_idx, fabrik_data_chain.size(), "FABRIK joint out of range!");
-	fabrik_data_chain.write[p_joint_idx].use_target_rotation = p_use_target_rotation;
+void SkeletonIK2DFABR::set_tip_use_target_rotation(bool p_use_target_rotation) {
+	tip_use_target_rotation = p_use_target_rotation;
 }
 
-bool SkeletonIK2DFABR::get_fabrik_joint_use_target_rotation(int p_joint_idx) const {
-	ERR_FAIL_INDEX_V_MSG(p_joint_idx, fabrik_data_chain.size(), false, "FABRIK joint out of range!");
-	return fabrik_data_chain[p_joint_idx].use_target_rotation;
+bool SkeletonIK2DFABR::is_tip_use_target_rotation() const {
+	return tip_use_target_rotation;
 }
 
+void SkeletonIK2DFABR::set_joint_bones(Array p_node_paths) {
+	set_bone_chain_size(p_node_paths.size());
+	for (int i = 0; i < p_node_paths.size(); i++) {
+		set_joint_bone(i, p_node_paths[i]);
+	}
+	_update_bone_id();
+}
+Array SkeletonIK2DFABR::get_joint_bones() const {
+	Array ret;
+	for (int i = 0; i < fabrik_data_chain.size(); i++) {
+		ret.push_back(get_joint_bone(i));
+	}
+	return ret;
+}
+void SkeletonIK2DFABR::set_joint_magnets(PackedVector2Array p_magnets) {
+	int size = MIN(get_bone_chain_size(), p_magnets.size());
+	for (int i = 0; i < size; i++) {
+		set_joint_magnet(i, p_magnets[i]);
+	}
+}
+PackedVector2Array SkeletonIK2DFABR::get_joint_magnets() const {
+	PackedVector2Array ret;
+	for (int i = 0; i < fabrik_data_chain.size(); i++) {
+		ret.push_back(get_joint_magnet(i));
+	}
+	return ret;
+}
 void SkeletonIK2DFABR::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("set_target_node", "target_nodepath"), &SkeletonIK2DFABR::set_target_node);
+	ClassDB::bind_method(D_METHOD("set_target_node", "p_target_node"), &SkeletonIK2DFABR::set_target_node);
 	ClassDB::bind_method(D_METHOD("get_target_node"), &SkeletonIK2DFABR::get_target_node);
-
-	ClassDB::bind_method(D_METHOD("set_fabrik_data_chain_length", "length"), &SkeletonIK2DFABR::set_fabrik_data_chain_length);
-	ClassDB::bind_method(D_METHOD("get_fabrik_data_chain_length"), &SkeletonIK2DFABR::get_fabrik_data_chain_length);
-
-	ClassDB::bind_method(D_METHOD("set_fabrik_joint_bone2d_node", "joint_idx", "bone2d_nodepath"), &SkeletonIK2DFABR::set_fabrik_joint_bone2d_node);
-	ClassDB::bind_method(D_METHOD("get_fabrik_joint_bone2d_node", "joint_idx"), &SkeletonIK2DFABR::get_fabrik_joint_bone2d_node);
-	ClassDB::bind_method(D_METHOD("set_fabrik_joint_bone_index", "joint_idx", "bone_idx"), &SkeletonIK2DFABR::set_fabrik_joint_bone_index);
-	ClassDB::bind_method(D_METHOD("get_fabrik_joint_bone_index", "joint_idx"), &SkeletonIK2DFABR::get_fabrik_joint_bone_index);
-	ClassDB::bind_method(D_METHOD("set_fabrik_joint_magnet_position", "joint_idx", "magnet_position"), &SkeletonIK2DFABR::set_fabrik_joint_magnet_position);
-	ClassDB::bind_method(D_METHOD("get_fabrik_joint_magnet_position", "joint_idx"), &SkeletonIK2DFABR::get_fabrik_joint_magnet_position);
-	ClassDB::bind_method(D_METHOD("set_fabrik_joint_use_target_rotation", "joint_idx", "use_target_rotation"), &SkeletonIK2DFABR::set_fabrik_joint_use_target_rotation);
-	ClassDB::bind_method(D_METHOD("get_fabrik_joint_use_target_rotation", "joint_idx"), &SkeletonIK2DFABR::get_fabrik_joint_use_target_rotation);
+	ClassDB::bind_method(D_METHOD("get_bone_chain_size"), &SkeletonIK2DFABR::get_bone_chain_size);
+	ClassDB::bind_method(D_METHOD("set_bone_chain_size", "p_size"), &SkeletonIK2DFABR::set_bone_chain_size);
+	ClassDB::bind_method(D_METHOD("set_joint_bone", "p_joint_idx", "p_bone"), &SkeletonIK2DFABR::set_joint_bone);
+	ClassDB::bind_method(D_METHOD("get_joint_bone", "p_joint_idx"), &SkeletonIK2DFABR::get_joint_bone);
+	ClassDB::bind_method(D_METHOD("set_joint_bone_idx", "p_joint_idx", "p_bone_idx"), &SkeletonIK2DFABR::set_joint_bone_idx);
+	ClassDB::bind_method(D_METHOD("get_joint_bone_idx", "p_joint_idx"), &SkeletonIK2DFABR::get_joint_bone_idx);
+	ClassDB::bind_method(D_METHOD("set_joint_magnet", "p_joint_idx", "p_magnet_position"), &SkeletonIK2DFABR::set_joint_magnet);
+	ClassDB::bind_method(D_METHOD("get_joint_magnet", "p_joint_idx"), &SkeletonIK2DFABR::get_joint_magnet);
+	ClassDB::bind_method(D_METHOD("set_tip_use_target_rotation", "p_tip_use_target_rotation"), &SkeletonIK2DFABR::set_tip_use_target_rotation);
+	ClassDB::bind_method(D_METHOD("is_tip_use_target_rotation"), &SkeletonIK2DFABR::is_tip_use_target_rotation);
+	ClassDB::bind_method(D_METHOD("set_joint_bones", "p_node_paths"), &SkeletonIK2DFABR::set_joint_bones);
+	ClassDB::bind_method(D_METHOD("get_joint_bones"), &SkeletonIK2DFABR::get_joint_bones);
+	ClassDB::bind_method(D_METHOD("set_joint_magnets", "p_magnets"), &SkeletonIK2DFABR::set_joint_magnets);
+	ClassDB::bind_method(D_METHOD("get_joint_magnets"), &SkeletonIK2DFABR::get_joint_magnets);
 
 	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "target_node", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Bone2D"), "set_target_node", "get_target_node");
-	ADD_PROPERTY(PropertyInfo(Variant::INT, "fabrik_data_chain_length", PROPERTY_HINT_RANGE, "0, 100, 1"), "set_fabrik_data_chain_length", "get_fabrik_data_chain_length");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "bone_chain_size", PROPERTY_HINT_RANGE, "0, 100, 1"), "set_bone_chain_size", "get_bone_chain_size");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "tip_use_target_rotation"), "set_tip_use_target_rotation", "is_tip_use_target_rotation");
+
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "joint_bones", PROPERTY_HINT_ARRAY_TYPE, "NodePath"), "set_joint_bones", "get_joint_bones");
+	ADD_PROPERTY(PropertyInfo(Variant::PACKED_VECTOR2_ARRAY, "joint_magnets"), "set_joint_magnets", "get_joint_magnets");
 }
