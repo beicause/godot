3ca363f71021e55201f9e9c0ec27578743b17ba5
Luo Zhihao <luo_zhihao_jx@qq.com>
2024-07-29 02:10:18 +0800

    add missing flake pkgs

--- flake.nix
+++ flake.nix
@@ -9,19 +9,20 @@
     { nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
       # buildFHSEnv -> https://nixos.org/manual/nixpkgs/stable/#sec-fhs-environments
       # The packages included in appimagetools.defaultFhsEnvArgs are:
       # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/build-support/appimage/default.nix#L72-L208
-
       devShells.${system}.default =
         (pkgs.buildFHSEnv (
           pkgs.appimageTools.defaultFhsEnvArgs
           // {
             name = "dev";
-            # Packages installed in the development shell
             targetPkgs =
               pkgs: with pkgs; [
                 pkg-config
@@ -32,12 +33,19 @@
                 scons
                 cxx-rs
                 pre-commit
+                nodejs_22
+                rustup
+                cargo-ndk
+                jdk17
+                android-studio-tools
               ];
-            # Commands to be executed in the development shell
             profile = ''
               echo ------Godot C++ Scons Development Environment-------
-              IN_NIX_SHELL=1 NIX_SHELL_NAME=gd-env exec fish
+              IN_NIX_SHELL=1 NIX_SHELL_NAME=gd-env ANDROID_NDK_HOME=~/Android/ndk/23.2.8568313/ exec fish
             '';
+            # Install Android SDK:
+            # sdkmanager --sdk_root=<android_sdk_path> --licenses
+            # sdkmanager --sdk_root=<android_sdk_path> "platform-tools" "build-tools;34.0.0" "platforms;android-34" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;23.2.8568313"
           }
         )).env;
     };
