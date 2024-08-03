7270a2a1a6adbf6b35f62224316d4d5c3c491fb3
Luo Zhihao <luo_zhihao_jx@qq.com>
2024-08-03 13:27:42 +0800

    rust add windows

--- modules/a_rust/config.py
+++ modules/a_rust/config.py
@@ -1,9 +1,11 @@
-def can_build(env,platform):
-    env.module_add_dependencies("a_rust", ["a_lz4","a_jsonnet"])
-    return env["platform"] in ["android","linuxbsd"] and env["arch"] in ["arm64","x86_64"]
+def can_build(env, platform):
+    env.module_add_dependencies("a_rust", ["a_lz4", "a_jsonnet"])
+    return env["platform"] in ["android", "linuxbsd", "windows"] and env["arch"] in ["arm64", "x86_64"]
+
 
 def configure(env):
     pass
+
 
 def get_doc_classes():
     return [
@@ -12,6 +14,7 @@ def get_doc_classes():
         "JMESVariable",
         "JSONConverter",
     ]
+
 
 def get_doc_path():
     return "doc_classes"
