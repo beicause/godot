df48f0537f6d6783ea42df1ca61a6654deb0191b
Luo Zhihao <luo_zhihao_jx@qq.com>
2024-08-03 13:14:28 +0800

    disable cxxbridge warns

--- modules/a_rust/gd_glicol.h
+++ modules/a_rust/gd_glicol.h
@@ -1,8 +1,39 @@
-#pragma once
+/**************************************************************************/
+/*  gd_glicol.h                                                           */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#ifndef GD_GLICOL_H
+#define GD_GLICOL_H
 
 #include "core/object/ref_counted.h"
-#include "cxx.h"
-#include "gd_glicol.rs.h"
+#include "cxxbridge/cxx.h"
+#include "cxxbridge/gd_glicol.rs.h"
 
 class Glicol : public RefCounted {
 	GDCLASS(Glicol, RefCounted);
@@ -44,3 +75,5 @@ public:
 	void set_seed(float seed) { inst->set_seed(seed); };
 	void reset() { inst->reset(); };
 };
+
+#endif // GD_GLICOL_H
