02e448e9594645f6dbc83061847b3f815ebea2a2
Luo Zhihao <luo_zhihao_jx@qq.com>
2024-08-06 20:32:16 +0800

    Revert "svg: add lottie support and resource loader using thorvg"
    This reverts commit 5f5d937cdf5addc01336b2cb4869e7728c539433.

--- modules/svg/config.py
+++ modules/svg/config.py
@@ -4,10 +4,4 @@ def can_build(env, platform):
 
 
 def configure(env):
-    from SCons.Script import BoolVariable, Help, Variables
-
-    env_vars = Variables()
-    env_vars.Add(BoolVariable("lottie", "Enable Lottie support using thorvg", True))
-
-    env_vars.Update(env)
-    Help(env_vars.GenerateHelpText(env))
+    pass
