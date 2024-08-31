9f3c8f458f7e49959bdac50c05da38eaa2410c81
LuoZhihao <luo_zhihao_jx@qq.com>
2024-08-31 18:38:13 +0800

    rm LottieTexture. refactor lottie importer

--- modules/svg/config.py
+++ modules/svg/config.py
@@ -1,13 +1,16 @@
 def can_build(env, platform):
-    env.module_add_dependencies("lottie", ["a_lz4"])
     return True
 
 
 def configure(env):
-    from SCons.Script import BoolVariable, Help, Variables
+    pass
 
-    env_vars = Variables()
-    env_vars.Add(BoolVariable("lottie", "Enable Lottie support using thorvg", True))
 
-    env_vars.Update(env)
-    Help(env_vars.GenerateHelpText(env))
+def get_doc_classes():
+    return [
+        "ResourceImporterLottie",
+    ]
+
+
+def get_doc_path():
+    return "doc_classes"
