3dd5009e6a397d142158d9790ca60be59fbce9dd
LuoZhihao <luo_zhihao_jx@qq.com>
2024-09-11 22:19:35 +0800

    [a_sf] add

---
+++ modules/a_sf/register_types.cpp
@@ -0,0 +1,64 @@
+/**************************************************************************/
+/*  register_types.cpp                                                    */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include "register_types.h"
+#include "midi.h"
+#include "midi_buffer.h"
+#include "sound_font.h"
+
+Ref<ResourceFormatLoaderSoundFont> resource_loader_sf;
+Ref<ResourceFormatLoaderMidi> resource_loader_mid;
+
+void initialize_a_sf_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+	ClassDB::register_class<SoundFont>();
+	ClassDB::register_class<Midi>();
+	ClassDB::register_class<MidiBuffer>();
+
+	resource_loader_sf.instantiate();
+	resource_loader_mid.instantiate();
+
+	ResourceLoader::add_resource_format_loader(resource_loader_sf);
+	ResourceLoader::add_resource_format_loader(resource_loader_mid);
+}
+
+void uninitialize_a_sf_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+
+	ResourceLoader::remove_resource_format_loader(resource_loader_sf);
+	ResourceLoader::remove_resource_format_loader(resource_loader_mid);
+
+	resource_loader_sf.unref();
+	resource_loader_mid.unref();
+}
