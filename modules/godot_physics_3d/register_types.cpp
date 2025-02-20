c2185a0d6acfd5fce93503d29a87146c5199bed4
LuoZhihao <luo_zhihao_jx@qq.com>
2025-02-20 20:51:21 +0800

    Set default physical server

--- modules/godot_physics_3d/register_types.cpp
+++ modules/godot_physics_3d/register_types.cpp
@@ -51,7 +51,7 @@ void initialize_godot_physics_3d_module(ModuleInitializationLevel p_level) {
 		return;
 	}
 	PhysicsServer3DManager::get_singleton()->register_server("GodotPhysics3D", callable_mp_static(_createGodotPhysics3DCallback));
-	PhysicsServer3DManager::get_singleton()->set_default_server("GodotPhysics3D");
+	// PhysicsServer3DManager::get_singleton()->set_default_server("GodotPhysics3D");
 }
 
 void uninitialize_godot_physics_3d_module(ModuleInitializationLevel p_level) {
