7d6cc57ac59b7f5fa505523fef72cc9e8055f8d8
LuoZhihao <luo_zhihao_jx@qq.com>
2025-03-17 05:18:29 +0800

    Prompt to restart when project data (.godot) is missing

--- editor/editor_node.h
+++ editor/editor_node.h
@@ -410,6 +410,7 @@ private:
 
 	Tree *disk_changed_list = nullptr;
 	ConfirmationDialog *disk_changed = nullptr;
+	ConfirmationDialog *project_data_missing = nullptr;
 
 	bool scene_distraction_free = false;
 	bool script_distraction_free = false;
@@ -668,6 +669,8 @@ private:
 	void _load_error_dialog_visibility_changed();
 
 	void _execute_upgrades();
+
+	bool _is_project_data_missing();
 
 protected:
 	friend class FileSystemDock;
