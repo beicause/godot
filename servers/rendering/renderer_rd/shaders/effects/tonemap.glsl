f0d9ddf9a7c4662659f23e83d5a5004c93dafa73
LuoZhihao <luo_zhihao@outlook.com>
2025-07-15 02:00:27 +0800

    Blend glow in linear space

--- servers/rendering/renderer_rd/shaders/effects/tonemap.glsl
+++ servers/rendering/renderer_rd/shaders/effects/tonemap.glsl
@@ -905,10 +905,6 @@ void main() {
 
 	color.rgb = apply_tonemapping(color.rgb);
 
-	bool convert_to_srgb = bool(params.flags & FLAG_CONVERT_TO_SRGB);
-	if (convert_to_srgb) {
-		color.rgb = linear_to_srgb(color.rgb); // Regular linear -> SRGB conversion.
-	}
 #ifndef SUBPASS
 	// Glow
 	if (bool(params.flags & FLAG_USE_GLOW) && params.glow_mode != GLOW_MODE_MIX && params.glow_mode != GLOW_MODE_PRE_ADDITIVE) {
@@ -917,15 +913,16 @@ void main() {
 			glow = mix(glow, texture(glow_map, uv_interp).rgb * glow, params.glow_map_strength);
 		}
 
-		// high dynamic range -> SRGB
+		// high dynamic range -> LDR
 		glow = apply_tonemapping(glow);
-		if (convert_to_srgb) {
-			glow = linear_to_srgb(glow);
-		}
-
 		color.rgb = apply_glow(color.rgb, glow);
 	}
 #endif
+
+	bool convert_to_srgb = bool(params.flags & FLAG_CONVERT_TO_SRGB);
+	if (convert_to_srgb) {
+		color.rgb = linear_to_srgb(color.rgb); // Regular linear -> SRGB conversion.
+	}
 
 	// Additional effects
 
